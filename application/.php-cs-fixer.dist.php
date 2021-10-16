ad3369c570dc7d87091ed0eaca4dbac367ae25aa
katsushi takeuchi <tk1997220@gmail.com>
2021-10-17 06:40:23 +0900

    cs fixer

---
+++ application/.php-cs-fixer.dist.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database/factories',
+        __DIR__ . '/database/seeds',
+        __DIR__ . '/database/migrations',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ]);
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer:risky' => true,
+        'blank_line_after_opening_tag' => false,
+        'linebreak_after_opening_tag' => false,
+        'declare_strict_types' => true,
+        'phpdoc_types_order' => [
+            'null_adjustment' => 'always_last',
+            'sort_algorithm' => 'none',
+        ],
+        'no_superfluous_phpdoc_tags' => false,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'php_unit_test_case_static_method_calls' => [
+            'call_type' => 'this'
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+        'not_operator_with_successor_space' => true,
+    ])
+    ->setFinder($finder);
