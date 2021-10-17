eccf5c832c4f0acc9e5fb8ddd8978da4ecd5eafb
katsushi takeuchi <tk1997220@gmail.com>
2021-10-17 13:37:27 +0900

    update cs fixer

--- application/.php-cs-fixer.dist.php
+++ application/.php-cs-fixer.dist.php
@@ -17,9 +17,17 @@ return $config
     ->setRiskyAllowed(true)
     ->setRules([
         '@PhpCsFixer:risky' => true,
-        'blank_line_after_opening_tag' => false,
+        'blank_line_after_opening_tag' => true,
+        'array_syntax' => false,
+        'binary_operator_spaces' => true,
+        'blank_line_after_namespace' => true,
         'linebreak_after_opening_tag' => false,
-        'declare_strict_types' => true,
+        'declare_strict_types' => false,
+        'cast_spaces' => true,
+        'class_keyword_remove' => false,
+        'compact_nullable_typehint' => true,
+        'concat_space' => true,
+        'elseif' => true,
         'phpdoc_types_order' => [
             'null_adjustment' => 'always_last',
             'sort_algorithm' => 'none',
@@ -36,6 +44,12 @@ return $config
         'phpdoc_align' => [
             'align' => 'left',
         ],
+        'no_whitespace_before_comma_in_array' => true,
+        'no_leading_namespace_whitespace' => true,
+        'no_whitespace_in_blank_line' => true,
+        'no_empty_statement' => true,
+        'no_unused_imports' => true,
+        'no_trailing_whitespace'=> true,
         'not_operator_with_successor_space' => true,
     ])
     ->setFinder($finder);
